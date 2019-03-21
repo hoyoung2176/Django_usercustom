4dc4c15599a8423002f31ec20b548d369827a20e
hoyoung2176 <hoyoung2176@gamil.com>
2019-03-21 07:48:23 +0000

    04_customize form by formhelper

--- boards/forms.py
+++ boards/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from .models import Board
 # class BoardForm(forms.Form):
 #     title = forms.CharField(label='제목', widget=forms.TextInput(attrs={
@@ -39,4 +41,9 @@ class BoardForm(forms.ModelForm):
                                     },
                                                     
                             }
-        
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.add_input(Submit('submit', '작성!'))
--- myform/settings.py
+++ myform/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'boards.apps.BoardsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +120,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
