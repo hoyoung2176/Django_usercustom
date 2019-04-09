89a3b78685cd36826074200010fdce85f5d53a66
hoyoung2176 <hoyoung2176@gamil.com>
2019-04-09 01:42:15 +0000

    05_Authentication

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.models import User
+
+class UserCustomChangeForm(UserChangeForm):
+    class Meta:
+        # https://docs.djangoproject.com/en/2.2/ref/contrib/auth/
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name',]
